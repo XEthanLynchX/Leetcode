eff1fecf34e82905e7e274c498cf1123e9f3e274
Ethan Lynch <ethanlynch72@gmail.com>
2023-10-21 23:57:15 -0400

    comments

--- Two_pointers/ThreeSum.py
+++ Two_pointers/ThreeSum.py
@@ -27,25 +27,28 @@
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort
+        res = []  # Initialize an empty list to store the result (lists of unique triplets).
+        nums.sort  # This is a typo, it should be 'nums.sort()' to sort the input list.
 
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i=1]
-                continue
+        for i, a in enumerate(nums):  # Loop through the sorted list using its index and value.
+            if i > 0 and a == nums[i-1]:  # Check if the current value is the same as the previous one.
+                continue  # If it is, skip to the next iteration to avoid duplicates.
 
-            l, r = i + 1, len(nums) - 1
-            while l < r 
-                threesum = a + nums[l] + nums[r]
+            l, r = i + 1, len(nums) - 1  # Initialize left and right pointers for two-sum check.
+            while l < r:  # While the left pointer is less than the right pointer.
+                threeSum = a + nums[l] + nums[r]  # Calculate the sum of the three values.
+
                 if threeSum > 0:
-                    r-=1 
-                elif threeSum < 0: 
-                    l +=1 
-                else: 
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
+                    r -= 1  # If the sum is positive, decrement the right pointer.
+                elif threeSum < 0:
+                    l += 1  # If the sum is negative, increment the left pointer.
+                else:
+                    res.append([a, nums[l], nums[r]])  # If the sum is zero, add the triplet to the result list.
+                    l += 1  # Move the left pointer to the right.
                     while nums[l] == nums[l-1] and l < r:
-                        l += 1
-        return res
+                        l += 1  # Skip duplicate values to avoid duplicate triplets.
+
+        return res  # Return the list of unique triplets that sum to zero.
+
 
                     
